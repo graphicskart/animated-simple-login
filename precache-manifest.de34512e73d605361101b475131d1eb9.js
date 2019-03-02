35cc22849923dd5c41b2b5e985dd5f20bf58b790
graphicskart <graphicskart2018@gmail.com>
2019-03-03 00:59:53 +0530

    Updates

---
+++ precache-manifest.de34512e73d605361101b475131d1eb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e524d04f9daba50c3a61",
+    "url": "/animated-simple-login/static/js/runtime~main.e524d04f.js"
+  },
+  {
+    "revision": "ff78cefb597105a825e9",
+    "url": "/animated-simple-login/static/js/main.ff78cefb.chunk.js"
+  },
+  {
+    "revision": "3e3326a741f3aeca662f",
+    "url": "/animated-simple-login/static/js/2.3e3326a7.chunk.js"
+  },
+  {
+    "revision": "ff78cefb597105a825e9",
+    "url": "/animated-simple-login/static/css/main.a2208ab5.chunk.css"
+  },
+  {
+    "revision": "2365920fdddb2661cd4a3bea49b4ecfa",
+    "url": "/animated-simple-login/index.html"
+  }
+];
